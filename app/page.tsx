7729455ba9b8a3dee99eb5413881688cc10260ad
ToolNovaAI <rishabhgoko@gmail.com>
2026-08-04 22:37:38 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,23 +4,34 @@ import FeaturedProjects from "@/components/sections/FeaturedProjects";
 import Services from "@/components/sections/Services";
 import WhyChooseMe from "@/components/sections/WhyChooseMe";
 import Process from "@/components/sections/Process";
+import Technologies from "@/components/sections/Technologies";
 import Testimonials from "@/components/sections/Testimonials";
 
 export default function HomePage() {
   return (
-    <main className="overflow-hidden">
+    <main className="relative overflow-hidden">
+      {/* Hero */}
       <Hero />
 
+      {/* About */}
       <About />
 
+      {/* Featured Projects */}
       <FeaturedProjects />
 
+      {/* Services */}
       <Services />
 
+      {/* Why Choose Me */}
       <WhyChooseMe />
 
+      {/* Work Process */}
       <Process />
 
+      {/* Technology Stack */}
+      <Technologies />
+
+      {/* Testimonials */}
       <Testimonials />
     </main>
   );
