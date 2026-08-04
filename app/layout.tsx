944ef44a4c99add66bd65540a4189df10bb15884
ToolNovaAI <rishabhgoko@gmail.com>
2026-08-04 21:59:42 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,7 @@ const inter = Inter({
   subsets: ["latin"],
   variable: "--font-inter",
 });
+import Navbar from "@/components/layout/Navbar";
 
 const spaceGrotesk = Space_Grotesk({
   subsets: ["latin"],
